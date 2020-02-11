a3640da540dfbc2a23c454cea9d62a5082f67a48
Jayderade <30771579+Jayderade@users.noreply.github.com>
2020-02-11 16:30:07 +1100

    spin to win

---
+++ Assets/Scripts/Customer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Customer : MonoBehaviour
+{
+    public Rigidbody player;
+    public float rotateSpeed = 1;
+    public float maxRotate = 100000;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       /* if (rotateSpeed < maxRotate)
+        {
+            rotateSpeed = rotateSpeed + 1;
+        }
+        //player.transform.localScale = new Vector3(rotateSpeed * .0005f, rotateSpeed * .001f, rotateSpeed * .0005f);            
+            player.transform.Rotate(0, rotateSpeed * Time.deltaTime, 0);
+       // player.AddForce(transform.up * rotateSpeed * Time.deltaTime);
+    */}
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,9 +5,14 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public bool pickedUp = false;
-    public float speed;    
+    public float rotateSpeed;
+    public float speed;
+    public Transform tableSpace;
     public Transform foodSpace;  
     public Rigidbody player;
+    public Transform curFood;
+    public GameObject gum;
+    public Collider gumCol;
 
 
     // Start is called before the first frame update
@@ -15,42 +20,84 @@ public class Movement : MonoBehaviour
     {
         
     }
-
-    private void OnCollisionEnter(Collision collision)
+    #region Food pickup & down
+    public void OnCollisionEnter(Collision collision)
     {
         if (collision.transform.tag == "Food" && !pickedUp)
         {
+            
+            gum = collision.gameObject;
+            gumCol = gum.GetComponent<Collider>();
+            gumCol.enabled = false;
             collision.transform.position = foodSpace.position;
             collision.transform.SetParent(foodSpace);
             pickedUp = true;
         }
+        if (collision.transform.tag == "Table" && pickedUp)
+        {
+            collision.gameObject.tag = "Table Served";
+            tableSpace = collision.transform.GetChild(0);
+            gum.transform.position = tableSpace.position;
+            gum.transform.SetParent(collision.transform);
+            gum = null;
+            
+            pickedUp = false;
+        }
+        if (collision.transform.tag == "Table Served" && pickedUp)
+        {
+            collision.gameObject.tag = "Untagged";
+            tableSpace = collision.transform.GetChild(2).GetChild(0);
+            gum.transform.position = tableSpace.position;
+            gum.transform.SetParent(collision.transform);
+            gum = null;
 
-       
+            pickedUp = false;
+        }
+        if (collision.transform.tag == "Customer" && pickedUp)
+        {
+            collision.gameObject.tag = "Customer Hand";
+            tableSpace = collision.transform.GetChild(2).GetChild(0);
+            gum.transform.position = tableSpace.position;
+            gum.transform.SetParent(collision.transform);
+            gum = null;
+
+            pickedUp = false;
+        }
+        if (collision.transform.tag == "Customer Hand" && pickedUp)
+        {
+            collision.gameObject.tag = "Untagged";
+            tableSpace = collision.transform.GetChild(2).GetChild(1);
+            gum.transform.position = tableSpace.position;
+            gum.transform.SetParent(collision.transform);
+            gum = null;
+
+            pickedUp = false;
+        }
     }
-
+    #endregion
     // Update is called once per frame
     void Update()
     {
         #region Movement
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            player.AddForce(0,0, speed * Time.deltaTime);
+            player.AddForce(transform.forward * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
-            player.AddForce(0, 0,-speed * Time.deltaTime);
+            player.AddForce(transform.forward * -speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
-            player.AddForce(-speed * Time.deltaTime, 0, 0);
-            player.transform.Rotate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0);
+            //player.AddForce(-speed * Time.deltaTime, 0, 0);
+            player.transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0);
         }
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            player.transform.Rotate(0, Input.GetAxis("Horizontal") * speed * Time.deltaTime,0);
-            player.AddForce(speed * Time.deltaTime, 0, 0);
+            player.transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime,0);
+            //player.AddForce(speed * Time.deltaTime, 0, 0);
         }
         #endregion
-
+        
     }
 }
